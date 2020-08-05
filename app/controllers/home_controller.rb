e5e7cb895559b9ece77ff96e0ccc83ebecfe77f7
Santiago Quintana <san3091@gmail.com>
2020-08-04 19:10:30 -0500

    add pretty pictures thanks imre

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,9 @@ class HomeController < ApplicationController
   def index
     @contact_request = ContactRequest.new
   end
+
+  def download_booklet
+    send_file "#{Rails.root}/public#{helpers.asset_path("ride-or-wait-booklet.pdf")}", type: "application/pdf", filename: "ride-or-wait-booklet.pdf"
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   post '/contact-requests', to: 'contact_requests#create'
+  get 'home/download_booklet'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
